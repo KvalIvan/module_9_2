e0ff7b734c22dfb3ae48e461509fdf461405eff6
KvalIvan <serga.vanya14@gmail.com>
2024-10-01 17:28:38 +0300

    Add files via upload

--- module_9_2.py
+++ module_9_2.py
@@ -1,14 +1,12 @@
 first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
 second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
-third_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable', 'Task', 'Git', 'Comprehension',
-                 'Java', 'Computer', 'Assembler']
 
 first_result = [len(first_string) for first_string in first_strings if len(first_string) > 5]
 
 second_result = [(first_string, second_string) for first_string in first_strings for second_string in second_strings
                  if len(first_string) == len(second_string)]
 
-third_result = [{third_string: len(third_string)} for third_string in third_strings if not len(third_string) % 2]
+third_result = {word: len(word) for word in first_strings + second_strings if not len(word) % 2}
 
 print(first_result)
 print(second_result)
